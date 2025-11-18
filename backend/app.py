88f3d00787cf43f20a066aab7685ce53c0a8e472
ChPurna2003 <chpurna2003@gmail.com>
2025-11-19 00:38:48 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -21,6 +21,13 @@ def create_app():
 
     db.init_app(app)
 
+    from backend.db_init import seed_data
+
+    with app.app_context():
+    db.create_all()
+    seed_data()
+
+
     # ----------------------------------------------------
     # LOGIN MANAGER
     # ----------------------------------------------------
